a3384f6c5ad9d264877b59ea1392d9e9505ecb6e
Croquignolex <gpetitalex10@gmail.com>
2021-08-06 10:36:51 +0100

    Clean router logic

--- src/router.js
+++ src/router.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 
-import asyncComponent from './components/asyncComponent';
+import './assets/scss/loader.scss';
+
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const LoginPage = lazy(() => import('./containers/LoginContainer'));
+
+// page loader
+const PageLoader = () => {
+    return <div className="lds-ripple"><div/><div/></div>
+}
 
 // Component
-function AppRoutes({history}) {
+const AppRoutes = ({history}) => {
     return (
         <ConnectedRouter history={history}>
-            <Switch>
-                {/* Available pages on guest mode */}
-                <Route
-                    exact
-                    path="/"
-                    component={asyncComponent(() => import('./containers/LoginContainer'))}
-                />
-                {/* 404 page */}
-                <Route component={asyncComponent(() => import('./pages/NotFoundPage'))} />
-            </Switch>
+            <Suspense  fallback={<PageLoader />}>
+                <Switch>
+                    <Route exact path="/" component={LoginPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </Suspense>
         </ConnectedRouter>
     );
 }
