a008493efadb9955da79df2fc7d6bc18766a384b
Croquignolex <gpetitalex10@gmail.com>
2021-08-09 11:28:10 +0100

    ok

---
+++ src/shared/DomTitle.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import PropTypes from "prop-types";
+import {Helmet} from "react-helmet";
+
+const DomTitle = ({title}) => <Helmet><title>{title}</title></Helmet>;
+
+// Prop types to ensure destroyed props data type
+DomTitle.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+export default React.memo(DomTitle);
